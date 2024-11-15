25227d19b2b623e2515e9b0808a0e7bc8ec2ae0c
Omar Abou <omarabou0503@gmail.com>
2024-11-14 21:27:04 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import 'chartjs-plugin-datalabels';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
 
 const CostComparisonChart = () => {
   const data = [
@@ -8,8 +10,10 @@ const CostComparisonChart = () => {
     { name: 'Proposed Solution', value: 105495 },
   ];
 
+  
   return (
-    <BarChart width={600} height={400} data={data}>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '400px' }}>
+    <BarChart width={650} height={400} data={data}>
       <XAxis dataKey="name" />
       <YAxis type="number" domain={[0, 'dataMax']} />
       <CartesianGrid strokeDasharray="3 3" />
@@ -17,6 +21,9 @@ const CostComparisonChart = () => {
       <Legend />
       <Bar dataKey="value" fill="#8884d8" />
     </BarChart>
+    </div>
+
+    
   );
 };
 
