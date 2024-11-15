efa635a78a2e37f9af044a74d57f0d73dcda4ead
Omar Abou <omarabou0503@gmail.com>
2024-11-14 20:04:26 -0500

    Adding files

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
+const CostComparisonChart = () => {
+  const data = [
+    { name: 'Insperity', value: 600000 },
+    { name: 'ADP', value: 240000 },
+    { name: 'Proposed Solution', value: 9994 },
+  ];
+
+  return (
+    <BarChart width={600} height={400} data={data}>
+      <XAxis dataKey="name" />
+      <YAxis type="number" domain={[0, 'dataMax']} />
+      <CartesianGrid strokeDasharray="3 3" />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="value" fill="#8884d8" />
+    </BarChart>
+  );
+};
+
+export default CostComparisonChart;
